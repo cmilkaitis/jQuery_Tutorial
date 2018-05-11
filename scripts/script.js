bab2d90a605497eb1e6448dd2353d0363e34be23
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-11 11:20:49 -0400

    Filters

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,22 @@
-//Reach into the document and manipulate the elements
+//refining the selectors to get specific
 
-$("h3").css({border: "3px solid blue"});
 
-$(".wrapper").css({border: "3px solid red"});
+$("header nav li:first").css({border: "2px solid red"});
+$("header nav li:last").css({border: "2px solid red"});
 
-$("#clients").css({border: "3px solid yellow"});
+$("#contact ul:first").css({border: "2px solid red"});
+
+//overrides our first/last css
+$("header nav li:even").css({border: "2px solid blue"});
+$("header nav li:odd").css({border: "2px solid yellow"});
+
+$("section:not('#contact')").css({border: "2px solid green"});
+
+$("#social-nav li:lt(3)").css({border: "2px solid blue"});
+$("#social-nav li:gt(2)").css({border: "2px solid red"});
+
+//attribute filters
+$("div[class]").css({border: "2px solid pink"});
+
+//attribute with a specific value
+$("img[alt=quote]").css({border: "2px solid yellow"});
