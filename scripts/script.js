7c195658119cd51328f479f2a0f2d198080928b9
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-11 11:51:16 -0400

    Adding content with jQuery

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
-//Chaining
+//Adding content
 
-$("#contact-methods").css({border: "3px solid red"})
-                     .next().css({border: "3px solid green"})
-                     .closest("section").css({border: "3px solid blue"});
+var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'>The big fight live: Ham vs CHeese!</div>";
+
+$("#tweets div").append(tweet);
+
+
+
+/*
+
+.append() adds content to the bottom of the element
+.prepend() to the top of the element
+.before() before the element
+.after() after the element
+.html() changes the whole html of the element (replaces what was there)
+.text() changes the text of an element (use while tweet var does not have html in it)
+
+*/
