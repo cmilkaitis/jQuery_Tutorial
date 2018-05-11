3e95503f3d2da7f45a25f423b5c59cf7c74d532d
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-11 11:40:27 -0400

    Chaining

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,5 @@
-//using the next method to select the NEXT element and make changes
-$("#contact-methods").next().css({border: "3px solid red"}); //makes changes to the img element under the ul with id of contact-methods
+//Chaining
 
-$("#social-nav").prev().css({border: "3px solid red"}); //access the same element but moves UP
-
-$(".banner-title").parents().css({border: "3px solid pink"}); // bubbles up and selects all the parents
-
-$("#contact").find(".facebook").css({border: "3px solid purple"}); //faster than using ("#contact .facebook")
-
-//get socialnav id then get closest parent, a wrapper class
-$("#social-nav").closest(".wrapper").css({border: "3px solid orange"});
+$("#contact-methods").css({border: "3px solid red"})
+                     .next().css({border: "3px solid green"})
+                     .closest("section").css({border: "3px solid blue"});
