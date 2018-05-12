2252028af9dab11795394b4b676bf422c55ac3da
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-12 11:22:27 -0400

    Removing Content

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,9 @@
-//Wrap and unwrap elements 
+//Removing content
 
-/*
-
-$("section").wrap("<div>"); //wraps all sections individually with a div
-$("section").unwrap(); // will unwrap anything - doenst need an arguement
-$("section").wrapAll("<div>"); //wraps all sections non individually
+/* 
+.empty(); empties the innerHTML
+.remove(); removes the entire element
 
 */
 
-var wrapper = "<div class ='wrapper'>";
-var button = $(".button");
-var wrapped = true;
-
-button[0].onclick = function() {
-    if (wrapped) {
-        $("section").unwrap();
-        wrapped = false;
-        button.text("Wrap");
-    } else {
-        $("section").wrapAll(wrapper);
-        wrapped = true;
-        button.text("Unwrap");
-    }
-
-};
+$(".button").remove();
