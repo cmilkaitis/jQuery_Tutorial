e27dc6ebd441851046a1c392bec6483cfe2e73fb
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-12 13:15:46 -0400

    Lesson 19

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,10 @@
-//Lesson 16 Event Helpers
+//Lesson 19 Animations in jQuery
 
-$("#lead-banner").on("dblclick", function(){
-    alert("you double clicked me, how dare you");
-    $("#lead-banner").off("dblclick");
-});
+$("section > h2").on("click", function(){
+    $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", function(){
 
-//provides the same functionality of .dblclick or .click
-
-
-//Lesson 17 Document Reaady vs Window Load
-
-// will allow the scripts to be placed in the header instead of the closing of the body
-/*
-$("document").on("ready", function(){
+        alert("Animation complete");
+    });
+    //can only animate items with numerical values
 
 });
-
-//shorter version
-$("document").ready(function(){
-
-});
-
-//even shorter version
-$(function(){
-
-});
-
-//window on load waits for everything has loaded on the page, not just the DOM
-
-$(window).on("load", function(){
-
-});
-
-//shorthand
-$(window).load(function() {
-
-});
-*/
-
-//Lesson 18 The Event Object in jQuery
-$("*").on("click", function(e){
-    console.log(e.target); // onclick will log the node info
-    console.log("The event type is: " + e.type); // will also log the type of element
-    console.log("X co-ordinate of the event is: " + e.pageX); // will also log the mouse position on the X coordinate plane
-    console.log("Y co-ordinate of the event is: " + e.pageY); // will also log the mouse position on the Y coordinate plane
-    e.stopPropagation(); //stops this even from bubbling up to the parents of the clicked target
-});
-
-
