3d72240deaf8b98228ab509c0aa74114dd8cc9db
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-12 12:19:42 -0400

    Lesson14

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-//CSS with jQuery
+//Lesson 13 CSS with jQuery
 
 //this style is fine for 1 or 2 value changes but not more many
 $("#social-nav").css("top", "-200px").css("left", "100px");
@@ -14,4 +14,15 @@ $("#social-nav").css({
 
 
 
+//Lesson14 Adding & Removing Classes
 
+$("header .wrapper").removeClass("wrapper");
+$("header > div").addClass("wrapper"); //get the header get the direct child of that header
+
+//when button is clicked, show items
+var button = $("#lead-banner a");
+//unwrap from jQuery with bracket notation
+button[0].onclick = function() {
+    $("#points-of-sale").toggleClass("open");
+    return false; //turns off default behavior of the anchor tag
+};
