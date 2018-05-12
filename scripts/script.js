8592f69c30849dcf85f8bf930e940242e757ec4b
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-12 12:48:04 -0400

    Lesson16

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,10 @@
-//Lesson 13 CSS with jQuery
+//Lesson 16 Event Helpers
 
-//this style is fine for 1 or 2 value changes but not more many
-$("#social-nav").css("top", "-200px").css("left", "100px");
-
-
-
-$("#social-nav").css({
-    "top" : "-400px",
-    "left" : "150px",
-    "opacity" : "0.5",
-    "border-top" : "4px solid red"
+$("#lead-banner").on("dblclick", function(){
+    alert("you double clicked me, how dare you");
+    $("#lead-banner").off("dblclick");
 });
 
+//provides the same functionality of .dblclick or .click
 
 
-//Lesson14 Adding & Removing Classes
-
-$("header .wrapper").removeClass("wrapper");
-$("header > div").addClass("wrapper"); //get the header get the direct child of that header
-
-//when button is clicked, show items
-var button = $("#lead-banner a");
-//unwrap from jQuery with bracket notation
-button[0].onclick = function() {
-    $("#points-of-sale").toggleClass("open");
-    return false; //turns off default behavior of the anchor tag
-};
-
-
-//Lesson15 binding and Unbinding Events
-
-var myLis = $("#points-of-sale li");
-
-myLis.on("click", function() {
-    $(this).css({"background" : "pink"});
-
-    myLis.off("click"); //doesnt work like in the video - multiple checks by removing other lessons code Maybe a jQuery 1 feature that was removed in this version?
-});
