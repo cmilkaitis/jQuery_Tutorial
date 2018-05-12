b0b2ad3785f340da8c995619baa93217a7b26ec0
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-12 12:54:17 -0400

    Lesson 17

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,31 @@ $("#lead-banner").on("dblclick", function(){
 //provides the same functionality of .dblclick or .click
 
 
+//Lesson 17 Document Reaady vs Window Load
+
+// will allow the scripts to be placed in the header instead of the closing of the body
+
+$("document").on("ready", function(){
+
+});
+
+//shorter version
+$("document").ready(function(){
+
+});
+
+//even shorter version
+$(function(){
+
+});
+
+//window on load waits for everything has loaded on the page, not just the DOM
+
+$(window).on("load", function(){
+
+});
+
+//shorthand
+$(window).load(function() {
+
+});
