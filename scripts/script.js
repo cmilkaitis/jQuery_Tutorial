d29cb46158a5f87c37696f0b10ee746adade75c5
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-14 10:54:30 -0400

    Lesson 25

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,5 @@
-// Lesson 22 - Sliding Elements
-/*
-$(".slide-button-up").on("click", function(){
-    $("#lead-banner").slideUp();
-});
+//Lesson25 - PLUGINS
 
-$(".slide-button-down").on("click", function(){
-    $("#lead-banner").slideDown(function(){
-        alert("animation complete")
-    });
-});
-*/
-
-
-$(".slide-button-down").on("click", function(){
-    $("#lead-banner").slideToggle();
-});
-
-
-//Lesson23 - Fading Animation Example
-var allQuotes = $("blockquote");
-var currentQuote = 0;
-
-function changeQuote(){
-    $(allQuotes[currentQuote]).fadeOut(200, function(){
-        if(currentQuote === allQuotes.length - 1) {
-            currentQuote = 0;
-        } else {
-            currentQuote++;
-        }
-    
-        $(allQuotes[currentQuote]).fadeIn(200);
-    });
-}
-
-var quoteTimer = setInterval(changeQuote, 3000);
-
-
-//Lesson24 Sliding Animation Example
-
-var items = $("#points-of-sale li");
-
-items.on("click", function(){
-    $(this).find("p").slideToggle(500);
-})
+$(function() {
+    $(".rslides").responsiveSlides();
+  });
