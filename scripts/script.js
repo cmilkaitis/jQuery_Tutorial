2cebda4ae51defb895eb70ca3c839d320520fc3a
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-14 10:58:18 -0400

    Lesson26

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,18 @@
 $(function() {
     $(".rslides").responsiveSlides();
   });
+
+//Lesson26 WHATS NEXT????
+
+
+/* 
+
+Look at jQuery UI - helps make imporvements to user interface
+
+Experiement to create interactivity: Sliders, Tooltips, Expandable lists, Image galleries
+
+AJAX - Asynchronous JavaScript and XML: Communicates with a server and load data into a page without refreshing ie GoogleMaps
+
+more jQuery Plugins: and how to create your own
+
+*/
